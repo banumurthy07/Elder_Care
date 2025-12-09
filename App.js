dd8e2142caa1db2c603c62a925d5a8e06c1e61e5
banumurthy07 <banumurthy2007@gmail.com>
2025-12-09 19:29:05 +0530

    Refactor screen imports and set initial route to Login
    Updated import paths for screens and changed initial route to 'Login'.

--- App.js
+++ App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import HomeScreen from './screens/HomeScreen';
-import SeniorDashboard from './screens/SeniorDashboard';
-import CaregiverDashboard from './screens/CaregiverDashboard';
-import UserDashboard from './screens/UserDashboard';
-import CameraScreen from './screens/CameraScreen';
+import LoginScreen from './LoginScreen';
+import SignupScreen from './SignupScreen';
+import HomeScreen from './HomeScreen';
+import SeniorDashboard from './SeniorDashboard';
+import CaregiverDashboard from './CaregiverDashboard';
+import UserDashboard from './UserDashboard';
+import CameraScreen from './CameraScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SeniorDashboard" component={SeniorDashboard} />
         <Stack.Screen name="CaregiverDashboard" component={CaregiverDashboard} />
@@ -24,5 +25,5 @@ export default function App() {
         <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  );
 }
